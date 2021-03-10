e103c36662b283c7e635e51a64addf60c9f4e6d9
Kayla M McKay <kaymckay@caen-vnc-vm16.engin.umich.edu>
2021-03-10 10:54:32 -0500

    caen run

--- env/bin/activate.csh
+++ env/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/kdog/Documents/SenWintSem/486/assignment3/env"
+setenv VIRTUAL_ENV "/afs/umich.edu/user/k/a/kaymckay/Documents/486_assign3/env"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kdog/Documents/SenWintSem/486/assignment3/env"
+set -gx VIRTUAL_ENV "/afs/umich.edu/user/k/a/kaymckay/Documents/486_assign3/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,7 +52,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(env) "
+        if test -n "(env) "            
             printf "%s%s" "(env) " (set_color normal)
         else
             # ...Otherwise, prepend env
